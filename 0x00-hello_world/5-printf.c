7554a2779be5cca183fbad194695ca47be9e2717
Yohannes808 <yohannesapt@gmail.com>
2022-04-07 11:44:40 -0700

    commit done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+
+*main - prints the the phrase with proper grammer,
+
+*but the outcome is a piece of art
+
+*/
+
+int main(void)
+
+{
+
+        printf("with proper grammer, but the outcome is apiece of art,\n");
+
+        return (0);
+
+}
