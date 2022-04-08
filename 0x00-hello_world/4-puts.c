a4c6b02dffb9e1a1e5d40b7a8355de4bd5aaf19e
Yohannes808 <yohannesapt@gmail.com>
2022-04-07 23:53:37 -0700

    commit done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-
-*main - entry point
-
-*
-
-*Return: 0 if exested properly, non zero otherwise  
-
-*/
-
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
-
 {
-
-puts(""Programming is like building a multilingual puzzle");
-
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
